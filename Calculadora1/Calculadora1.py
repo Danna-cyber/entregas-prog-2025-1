df9caac7e5107890c538d6843c20b472f34aea05
Danna-cyber <dannamotta0711@gmail.com>
2025-03-24 12:57:03 -0500

    Calculadora1.py

--- Calculadora1/Calculadora1.py
+++ Calculadora1/Calculadora1.py
@@ -1,65 +1,73 @@
 #!/usr/bin/env python3
 
-"""           
+"""
 Calculadora 1: prototipo basico
 
-Este es un ejemplo de demostración del uso de la plantilla para el curso de Introducción a la progración 
+Este es un ejemplo de demostración del uso de la plantilla
+para el curso de Introducción a la progración
 
 Autor: Danna Fernanda Motta Heranadez <dannamotta1306@gmail.com>
 Fecha: 2025-03-13
 """
 
-# **** En esta región puede importar los módulos necesarios para su programa
-# **** O las definiciones de clases y/o funciones que requiera.
+# ** En esta región puede importar los módulos necesarios para su programa
+# ** O las definiciones de clases y/o funciones que requiera.
 
 
 def run():
     """script entrypoint"""
 
-    # **** Poner el código ejecutable de su ejercicio aquí
-    
-print("Escoge una operación: ")
-print("A. Suma ")
-print("B. Resta ")
-print("C. Multiplicación ") 
-print("D. División ")
+    # ** Poner el código ejecutable de su ejercicio aquí
 
-escogerOperacion = input("Bienvenida, selecciona tu operación entre A/B/C/D: ")
+    print("Escoge una operación: ")
+    print("A. Suma ")
+    print("B. Resta ")
+    print("C. Multiplicación ")
+    print("D. División ")
 
-# Recopilar operandos
-primerTermino = int(input("Escribe un número entero: "))
-segundoTermino = int(input("Escribe otro número entero: "))
+    escogerOperacion = input("Bienvenida, selecciona tu operación entre A/B/C/D: ")
 
-if escogerOperacion == 'A':
-    resultadoSuma = primerTermino + segundoTermino
-    print(f"La suma de {primerTermino} y {segundoTermino} es {resultadoSuma}")
+    # Recopilar operandos
+    primerTermino = int(input("Escribe un número entero: "))
+    segundoTermino = int(input("Escribe otro número entero: "))
 
-elif escogerOperacion == 'B':
-    resultadoResta = primerTermino - segundoTermino
-    print(f"La resta de {primerTermino} y {segundoTermino} es {resultadoResta}")
+    if escogerOperacion == 'A':
+        resultadoSuma = primerTermino + segundoTermino
+        print(
+            f"La suma de {primerTermino} y {segundoTermino} es {resultadoSuma}"
+            )
 
-elif escogerOperacion == 'C':
-    resultadoMultiplicacion = primerTermino * segundoTermino
-    print(f"La multiplicación de {primerTermino} y {segundoTermino} es {resultadoMultiplicacion}")
+    elif escogerOperacion == 'B':
+        resultadoResta = primerTermino - segundoTermino
+        print(
+            f"La resta de {primerTermino} y {segundoTermino} es {resultadoResta}"
+            )
 
-elif escogerOperacion == 'D':
-    if segundoTermino != 0:
-        resultadoDivision = primerTermino / segundoTermino
-        print(f"La división de {primerTermino} y {segundoTermino} es {resultadoDivision}")
+    elif escogerOperacion == 'C':
+        resultadoMultiplicacion = primerTermino * segundoTermino
+        print(
+            f"La multiplicación de {primerTermino} y {segundoTermino} es {resultadoMultiplicacion}"
+            )
+
+    elif escogerOperacion == 'D':
+        if segundoTermino != 0:
+            resultadoDivision = primerTermino / segundoTermino
+            print(
+                f"La división de {primerTermino} y {segundoTermino} es {resultadoDivision}"
+                )
+        else:
+            print("Nunca dividas por cero 0.")
+
+    # Si no se escoge una opción de A/B/C/D, ejecutar de nuevo el código
     else:
-        print("Nunca dividas por cero 0.")
+        print("Selecciona opciones A/B/C/D.")
 
-# Si no se escoge una opción de A/B/C/D, ejecutar de nuevo el código 
-else:
-    print("Selecciona opciones A/B/C/D.")
+        # Saludo
+        print("Hola mundo!")
 
-
-  # Saludo
-    print("Hola mundo!")
-
-    # **** ****
+        # ** **
 
 
-# **** Conserve este condicional para ejecutar el programa directamente
+# ** Conserve este condicional para ejecutar el programa directamente
 if __name__ == "__main__":
     run()
